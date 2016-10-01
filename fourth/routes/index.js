884e0ea0efb257a983ccb1fde0ed41ced9ecbd50
katenokp p <katenokp.p@gmail.com>
2016-10-01 18:21:39 +0500

    вычисление веса

--- fourth/routes/index.js
+++ fourth/routes/index.js
@@ -9,6 +9,7 @@ router.get('/', function(req, res, next) {
     var data = readData(res);
     //console.log(data);
     //res.render('map', {data: data.id});
+    //todo сделать перед сохранением проверку на отсутствие одинаковых id
 });
 
 function readData(res){
@@ -20,9 +21,41 @@ function readData(res){
             var parsedData = JSON.parse(data);
             //return JSON.parse(data);
             //console.log()
+            parsedData.forEach(function(item){
+                calculateCompleteness(item);
+            });
             res.render('index', {data: parsedData});
         }
     })
+
+}
+
+function calculateCompleteness(data){
+    var result = {
+        all: 0,
+        done: 0
+    };
+    if(data.children == null){
+        if(data.weight == null){
+             result = {
+                all: 1,
+                done: data.isDone ? 1: 0
+            };
+
+        } else{
+            result = data.weight;
+        }
+    } else{
+        data.children.forEach(function(child){
+            var childrenWeight = calculateCompleteness(child);
+            result = {
+                all: result.all + childrenWeight.all,
+                done: result.done + childrenWeight.done
+            }
+        })
+    }
+    console.log("weight for %s: %d/%d", data.id, result.done, result.all);
+    return result;
 
 }
 
