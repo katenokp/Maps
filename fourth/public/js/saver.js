2c4e51313a6efa120f3490b7c42193fefdabbcaf
katenokp p <katenokp.p@gmail.com>
2016-10-06 14:45:29 +0500

    сохранение в исходный файл

--- fourth/public/js/saver.js
+++ fourth/public/js/saver.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 function saveToFile(serviceName, data, replacer){
-    var fullFileName = path.join("./data/" + ("saved" + serviceName + "Data.json")); //todo разобраться с правильным путём
+    var fullFileName = path.join("./data/" + (serviceName + "/data.json")); //todo разобраться с правильным путём
     console.warn("file path: %s", fullFileName);
     fs.writeFile(fullFileName, JSON.stringify(data, replacer, '\t'), {"encoding": 'utf8'}, function(error){
         if(error){
