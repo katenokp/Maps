1e4be27cf78fe43e80de89bca323b755c82987f6
katenokp p <katenokp.p@gmail.com>
2016-10-11 09:58:15 +0500

    поправила id

--- fourth/public/js/prepareData.js
+++ fourth/public/js/prepareData.js
@@ -30,7 +30,7 @@ function normalizeItemsArray(itemsArray){
 
 function prepareItem(item){
     if(item.id == null){
-        item.id = 'id' + getNewGuid();
+        item.id = getNewGuid();
     }
 
     var priority = item.priority;
