b57d9b8f42f3a9a13073fe10ca12370146e0d433
katenokp p <katenokp.p@gmail.com>
2016-10-19 17:16:51 +0500

    сохранение

--- public/js/saver.js
+++ public/js/saver.js
@@ -1,40 +1,34 @@
 var fs = require('fs');
 var path = require('path');
 var diff3 = require('node-diff3').diff;
-//var diff3 = require('diff3');
 
 function saveToFile(reqBody, replacer, callback){
     var serviceName = reqBody.service;
 
-    var oldDataFileName = path.join("./data/" + (serviceName + "/oldData.json")); //todo разобраться с правильным путём
-    var newDataFileName = path.join("./data/" + (serviceName + "/newData.json"));
+    var dataFileName = path.join("./data/" + (serviceName + "/data.json")); //todo разобраться с правильным путём
     var commonInformationFileName = path.join("./data/" + (serviceName + "/commonInformation.json"));
 
-    var oldData = JSON.stringify(reqBody.oldData, replacer, '\t').split('\n');
-    var newData = JSON.stringify(reqBody.data, replacer, '\t').split('\n');
+    fs.readFile(dataFileName, 'utf8', function(error, actualDataFile){
+        if(error)
+            throw error;
+        else{
+            var actualData = actualDataFile.split('\n');
+            var oldData = JSON.stringify(reqBody.oldData, replacer, '\t').split('\n');
+            var newData = JSON.stringify(reqBody.data, replacer, '\t').split('\n');
 
-    console.warn("file path: %s", newDataFileName);
-    fs.writeFile(newDataFileName, JSON.stringify(reqBody.data, replacer, '\t'), {"encoding": 'utf8'}, function(error){
-        if(error){
-            console.error(error);
-            return false;
-        } else{
-            console.log("New data saved to file %s", newDataFileName);{
-                fs.writeFile(oldDataFileName, JSON.stringify(reqBody.oldData, replacer, '\t'), {"encoding": "utf8"}, function(error){
-                    if(error)
-                        console.log(error);
-                    else{
-                        //var r0 = diff3.diffComm(oldData, newData);
-                        /*var r1 = diff3.diffPatch(oldData, newData);
-                        var r2 = diff3.patch(oldData, r1);
-                        //console.log(r0);
-                        console.log(r2);*/
-                        merge(oldDataFileName, newDataFileName);
-                    }
-                });
+            var patch = diff3.diffPatch(oldData, newData);
 
-            }
-            return saveCommonInformation(reqBody.weight, commonInformationFileName, callback);
+            actualData = diff3.patch(actualData, patch);
+
+            fs.writeFile(dataFileName, actualData, {"encoding": 'utf8'}, function(error){
+                if(error){
+                    console.error(error);
+                } else{
+                    console.log("New data saved to file %s", dataFileName);
+
+                    saveCommonInformation(reqBody.weight, commonInformationFileName, callback);
+                }
+            });
         }
     });
 }
@@ -46,17 +40,23 @@ function merge(oldDataFileName, newDataFileName){
     var testReplacer = ["name", "all", "priority"];
 
     var oldData = JSON.stringify({all: 1, priority: 3, name: "name"}, testReplacer, '\t').split('\n');
+    var oldData1 = JSON.stringify({all: 1, priority: 3, name: "name"}, testReplacer, '\t').split('\n');
     var first = JSON.stringify({all: 2, priority: 3, name: "name"}, testReplacer, '\t').split('\n');
     var second = JSON.stringify({all: 1, priority: 5, name: "name"}, testReplacer, '\t').split('\n');
 
-    var patch1 = diff3.diffPatch(oldData, first);
+    /*var patch1 = diff3.diffPatch(oldData, first);
     var patch2 = diff3.diffPatch(oldData, second);
 
     var afterFirst = diff3.patch(oldData, patch1);
     console.log(afterFirst);
 
     var afterSecond = diff3.patch(afterFirst, patch2);
-    console.log(afterSecond);
+    console.log(afterSecond);*/
+
+    console.log(diff3.diffIndices(oldData, oldData1).length);
+    console.log(diff3.diffIndices(oldData, first).length);
+    console.log(diff3.diffIndices(second, oldData1).length);
+    console.log(diff3.diffIndices(first, second).length);
 }
 
 function saveCommonInformation(data, fileName, callback){
