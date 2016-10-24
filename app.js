7baa2013c5130573850dd7b62af1cab6f7030efb
katenokp p <katenokp.p@gmail.com>
2016-10-24 14:32:16 +0500

    скрыла полный путь со страницы ошибки

--- app.js
+++ app.js
@@ -25,24 +25,24 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json({limit: '10mb'}));
 app.use(bodyParser.urlencoded({limit: '10mb'}));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
-app.post('/save', function(req, res){
+app.post('/save', function (req, res) {
     //var data = req.body;
     console.warn('post');
     var isDataOnServiceChanged = false;
     var saveDataStatus = saveData(req.body, replacer);
     //if(saveDataStatus){
-        res.status = 200;
-        res.send("saved");
+    res.status = 200;
+    res.send("saved");
     //} else{
-        //res.status = 500;
-        //res.send();
+    //res.status = 500;
+    //res.send();
     //}
 });
 
@@ -51,9 +51,8 @@ app.use('/converter', bodyParser.urlencoded({
 }));
 
 
-app.post('/converter', function(req, res, next){
-    if(req.body.service == null)
-    {
+app.post('/converter', function (req, res, next) {
+    if (req.body.service == null) {
         var result = JSON.stringify(parser(req.body.text), ["name", "children"], '\t');
         res.status = 200;
         //res.render('convert', {data: result});
@@ -62,7 +61,7 @@ app.post('/converter', function(req, res, next){
 
     else {
         var parsedData;
-        if(req.body.text[0] =='[' || req.body.text[0] == '{'){
+        if (req.body.text[0] == '[' || req.body.text[0] == '{') {
             parsedData = JSON.parse(req.body.text);
         } else {
             parsedData = parser(req.body.text)
@@ -77,27 +76,32 @@ app.post('/converter', function(req, res, next){
             }
         };
 
-        var saveDataStatus = saveData(data, replacer, function(){
+        var saveDataStatus = saveData(data, replacer, function () {
             //res.status = 200;
-            res.render('index', {data: data.data, weight: {all:0, done:0}, service: data.service, serviceName: servicesNames[data.service]})
+            res.render('index', {
+                data: data.data,
+                weight: {all: 0, done: 0},
+                service: data.service,
+                serviceName: servicesNames[data.service]
+            })
         });
 
 
         /*if (saveDataStatus) {
-            res.status = 200;
-            res.send();
-        } else {
-            res.status = 500;
-            res.send();
-        }*/
+         res.status = 200;
+         res.send();
+         } else {
+         res.status = 500;
+         res.send();
+         }*/
     }
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -105,24 +109,27 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  //app.locals.pretty = true
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    //app.locals.pretty = true
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        console.log(err.message);
+        console.log(err.stack);
+        res.render('error', {
+            message: err.message.replace(__dirname + '\\', '..'),
+            error: err,
+            dirname: __dirname + '\\'
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message.replace(__dirname, ''),
+        error: {}
+    });
 });
 
 
