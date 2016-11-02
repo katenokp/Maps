1f82079bc244b1aae9e20dc7a788bde2208f8404
katenokp p <katenokp.p@gmail.com>
2016-11-02 15:04:02 +0500

    переход к следующему найденному

--- public/js/searcher.js
+++ public/js/searcher.js
@@ -1,53 +1,67 @@
-function search(event){
-    if(event.keyCode != 13)
+function search(event) {
+    if (event.keyCode != 13)
         return;
     var value = document.getElementById("Search").value;
-    if(value.trim() == "")
-        clearResult();
+    if (value.trim() == "") {
+        clearFoundResult();
+        return;
+    }
+    if (localStorage.getItem("searchValue") != null && value != localStorage.getItem("searchValue")) {
+        clearFoundResult();
+    }
     var viewedResultNumber = localStorage.getItem("viewedResultNumber");
     var foundElementsIds;
-    if(viewedResultNumber == null){
+    if (viewedResultNumber == null) {
         foundElementsIds = searchAllElements(value);
         localStorage.setItem("foundElementsIds", JSON.stringify(foundElementsIds));
         localStorage.setItem("searchValue", value);
         localStorage.setItem("viewedResultNumber", "0"); //todo
-    } else{
+    } else {
         foundElementsIds = JSON.parse(localStorage.getItem("foundElementsIds"));
+        restorePreviousState();
     }
     var currentFoundElementId = nextValue(foundElementsIds);
     expandFoundElement(currentFoundElementId);
 }
 
-function expandFoundElement(foundElementId){
+function expandFoundElement(foundElementId) {
     var parentIds = getAllParentsIds(foundElementId);
     var changedElementsIds = [];
-    parentIds.forEach(function(item){
+    parentIds.forEach(function (item) {
         var isElementChanged = expandElementIfNeed(item);
-        if(isElementChanged){
+        if (isElementChanged) {
             changedElementsIds.push(item);
         }
     });
     localStorage.setItem("changedElementsIds", JSON.stringify(changedElementsIds));
 }
 
+function restorePreviousState() {
+    var changedElementsIds = JSON.parse(localStorage.getItem("changedElementsIds"));
+        changedElementsIds.forEach(function (item) {
+            collapseElementIfNeed(item);
+        });
+
+    localStorage.removeItem("changedElementsIds");
+}
 
 
-function nextValue(foundElementsIds){
+function nextValue(foundElementsIds) {
     var foundElementsCount = foundElementsIds.length;
     var viewedResultNumber = parseInt(localStorage.getItem("viewedResultNumber"));
-    if(viewedResultNumber == foundElementsCount - 1) {
+    if (viewedResultNumber == foundElementsCount - 1) {
         localStorage.setItem("viewedResultNumber", "0");
-        return foundElementsIds[foundElementsCount-1];
+        return foundElementsIds[foundElementsCount - 1];
     }
     localStorage.setItem("viewedResultNumber", viewedResultNumber + 1);
     return foundElementsIds[viewedResultNumber];
 }
 
-function searchAllElements(str){
+function searchAllElements(str) {
     var nodesNames = document.getElementsByClassName("itemName");
     var searchNodesNamesIds = [];
-    for(var i=0; i<nodesNames.length; i++){
-        if(nodeNameContainsStr(nodesNames[i], str)){
+    for (var i = 0; i < nodesNames.length; i++) {
+        if (nodeNameContainsStr(nodesNames[i], str)) {
             searchNodesNamesIds.push(getNodeId(nodesNames[i].id));
             console.log(nodesNames[i].innerHTML);
         }
@@ -55,17 +69,17 @@ function searchAllElements(str){
     return searchNodesNamesIds;
 }
 
-function nodeNameContainsStr(node, str){
+function nodeNameContainsStr(node, str) {
     var normalizedStr = normalizeStr(str);
     var normalizedNodeName = normalizeStr(node.innerHTML);
     return normalizedNodeName.includes(normalizedStr);
 }
 
-function normalizeStr(str){
+function normalizeStr(str) {
     return str.toLowerCase().trim().replace(/ /g, '');
 }
 
-function clearResult(){
+function clearFoundResult() {
     localStorage.removeItem("foundElementsIds");
     localStorage.removeItem("searchValue");
     localStorage.removeItem("viewedResultNumber");
