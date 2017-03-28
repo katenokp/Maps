50d57f4e05c2d99bf3be7faa8ca4392b724d21c4
katenokp p <katenokp.p@gmail.com>
2017-03-28 20:22:21 +0500

    отображение сохраненного юзера

--- js/users.js
+++ js/users.js
@@ -1,2 +1,12 @@
-
+function getUserName(userNumber){
+    switch (userNumber){
+        case 1 : return 'Alebastr';
+        case 2 : return 'e_pajl';
+        case 3 : return 'gsl';
+        case 4 : return 'nass_qwerty';
+        case 5 : return 'timkinatv';
+        case 6 : return 'tmv';
+        default : return 'default';
+    }
+}
 
